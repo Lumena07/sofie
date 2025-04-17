68e09d019b4464a5c5502101f4b84f23568e1679
Alice-K <alycelumena@gmail.com>
2025-04-17 11:38:17 +0300

    added and deleted files

--- api/health.py
+++
@@ -1,8 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-import json
-
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"status": "healthy"})
-    } 
